bae097c03885946f4df2cca651626bb935a9f583
itsAndL <ayoub003lkebch@gmail.com>
2023-03-31 14:05:10 +0000

    remove schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -59,18 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_050951) do
     t.index ["track_id"], name: "index_courses_on_track_id"
   end
 
-  create_table "courses_profiles", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "profile_id", null: false
-    t.bigint "course_id", null: false
-    t.index ["course_id", "profile_id"], name: "index_courses_profiles_on_course_id_and_profile_id"
-    t.index ["profile_id", "course_id"], name: "index_courses_profiles_on_profile_id_and_course_id"
-  end
-
-  create_table "homes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "lectures", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "youtube_video_link"
     t.datetime "created_at", null: false
@@ -282,13 +270,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_050951) do
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
-  create_table "profiles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "username"
-    t.text "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "quizzes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -332,15 +313,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_050951) do
     t.datetime "updated_at", null: false
     t.index ["scorable_type", "scorable_id"], name: "index_user_points_on_scorable"
     t.index ["user_id"], name: "index_user_points_on_user_id"
-  end
-
-  create_table "user_progress_histories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "step_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["step_id"], name: "index_user_progress_histories_on_step_id"
-    t.index ["user_id"], name: "index_user_progress_histories_on_user_id"
   end
 
   create_table "user_progresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -392,7 +364,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_050951) do
     t.string "unconfirmed_email"
     t.string "username"
     t.text "learning_goal"
-    t.string "avatar"
     t.string "provider"
     t.string "uid"
     t.string "avatar_url"
@@ -411,8 +382,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_050951) do
   add_foreign_key "motor_taggable_tags", "motor_tags", column: "tag_id"
   add_foreign_key "steps", "courses"
   add_foreign_key "user_points", "users"
-  add_foreign_key "user_progress_histories", "steps"
-  add_foreign_key "user_progress_histories", "users"
   add_foreign_key "user_progresses", "users"
   add_foreign_key "user_quiz_responses", "quizzes"
   add_foreign_key "user_quiz_responses", "users"
