51c6e1eb1b707439399ebe762406a6ee4ad249e0
itsAndL <ayoub003lkebch@gmail.com>
2023-04-01 14:31:34 +0000

    small change

--- db/migrate/20230401065214_add_basic_cols_to_users.rb
+++ db/migrate/20230401065214_add_basic_cols_to_users.rb
@@ -1,11 +1,11 @@
 class AddBasicColsToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :birthdate, :date
-    add_column :users, :start_date, :date
-    add_column :users, :end_date, :date
-    add_column :users, :gender, :integer
-    add_column :users, :marital_status, :integer
+  change_table :users do |t|
+    t.string :first_name
+    t.string :last_name
+    t.date :birthdate
+    t.date :start_date
+    t.date :end_date
+    t.integer :gender
+    t.integer :marital_status
   end
 end
