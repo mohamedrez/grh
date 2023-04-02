fb302195f9f05e8f2a151d97db464ec30df32527
itsAndL <ayoub003lkebch@gmail.com>
2023-04-02 17:23:05 +0000

    Full scaffold for the experience model

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,5 +1,5 @@
 class ExperiencesController < ApplicationController
-  before_action :set_experience, only: %i[ show edit update destroy ]
+  before_action :set_experience, only: %i[show edit update destroy]
 
   # GET /experiences or /experiences.json
   def index
@@ -25,7 +25,7 @@ class ExperiencesController < ApplicationController
 
     respond_to do |format|
       if @experience.save
-        format.html { redirect_to experience_url(@experience), notice: "Experience was successfully created." }
+        format.html { redirect_to experience_url(@experience), notice: t("experiences.experience_created") }
         format.json { render :show, status: :created, location: @experience }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class ExperiencesController < ApplicationController
   def update
     respond_to do |format|
       if @experience.update(experience_params)
-        format.html { redirect_to experience_url(@experience), notice: "Experience was successfully updated." }
+        format.html { redirect_to experience_url(@experience), notice: t("experiences.experience_updated") }
         format.json { render :show, status: :ok, location: @experience }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +52,20 @@ class ExperiencesController < ApplicationController
     @experience.destroy
 
     respond_to do |format|
-      format.html { redirect_to experiences_url, notice: "Experience was successfully destroyed." }
+      format.html { redirect_to experiences_url, notice: t("experiences.experience_destroyed") }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_experience
-      @experience = Experience.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def experience_params
-      params.require(:experience).permit(:job_title, :company_name, :employment_type, :start_date, :end_date, :work_description, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_experience
+    @experience = Experience.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def experience_params
+    params.require(:experience).permit(:job_title, :company_name, :employment_type, :start_date, :end_date, :work_description, :user_id)
+  end
 end
