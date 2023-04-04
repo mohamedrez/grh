8b30815b236e7acd151f906423cea51a0dc8a562
itsAndL <ayoub003lkebch@gmail.com>
2023-04-04 10:44:31 +0000

    Go with Rubocop feedback

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 
   scope "(:locale)", locale: /en|ar/ do
-    resources :educations
+    resources :educationsr
     resources :experiences
     resources :home, only: [:index]
 
