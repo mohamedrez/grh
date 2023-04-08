2c95509f2cdad1ba3dcf9bba3be8c1b8759883c4
itsAndL <ayoub003lkebch@gmail.com>
2023-04-08 06:57:23 +0000

    testing the request for editing the user info

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { create(:user) }
 
   describe "GET /users/:id/edit" do
     it "renders a successful response" do
@@ -10,10 +10,10 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  describe "PATCH /users/:id" do
-    let(:params) do
-      {
-        user: {
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:valid_attributes) do
+        {
           first_name: "John",
           last_name: "Doe",
           about: "<div>hello everyone</div>",
@@ -24,37 +24,42 @@ RSpec.describe "Users", type: :request do
           job_title: "RoR developer",
           start_date: "02-03-2019",
           end_date: "02-03-2024",
-          address_attributes: {
-            street: "123 Main St",
-            country: "usa",
-            city: "New York",
-            zipcode: "42157"
-          }
+          address_attributes:
+            {
+              street: "123 Main St",
+              country: "usa",
+              city: "New York",
+              zipcode: "42157"
+            }
         }
-      }
-    end
-    it 'updates the user' do
-      patch "/users/#{user.id}", params: params
-      expect(user.reload.first_name).to eq("John")
-      expect(user.reload.last_name).to eq("Doe")
-      expect(user.reload.about).to eq(user.about)
-      expect(user.reload.phone).to eq("12345678")
-      expect(user.reload.birthdate).to eq(Date.new(1998, 03, 02))
-      expect(user.reload.gender).to eq("male")
-      expect(user.reload.marital_status).to eq("single")
-      expect(user.reload.job_title).to eq("RoR developer")
-      expect(user.reload.start_date).to eq(Date.new(2019, 03, 02))
-      expect(user.reload.end_date).to eq(Date.new(2024, 03, 02))
+      end
 
-      expect(user.reload.address.street).to eq("123 Main St")
-      expect(user.reload.address.country).to eq("usa")
-      expect(user.reload.address.city).to eq("New York")
-      expect(user.reload.address.zipcode).to eq("42157")
+      it "updates the requested user" do
+        patch "/users/#{user.id}", params: {user: valid_attributes}
+        user.reload
 
-      params.update(user: { address_attributes: { country: "canada" } })
-      put "/users/#{user.id}", params: params
-      expect(user.reload.address.country).to eq("canada")
-      expect(user.reload.address.street).to eq("123 Main St")
+        expect(user.first_name).to eq("John")
+        expect(user.last_name).to eq("Doe")
+        expect(user.about).to eq(user.about)
+        expect(user.phone).to eq("12345678")
+        expect(user.gender).to eq("male")
+        expect(user.marital_status).to eq("single")
+        expect(user.job_title).to eq("RoR developer")
+        expect(user.start_date).to eq(Date.new(2019, 03, 02))
+        expect(user.birthdate).to eq(Date.new(1998, 03, 02))
+        expect(user.end_date).to eq(Date.new(2024, 03, 02))
+
+        expect(user.address.street).to eq("123 Main St")
+        expect(user.address.country).to eq("usa")
+        expect(user.address.city).to eq("New York")
+        expect(user.address.zipcode).to eq("42157")
+      end
+
+      it "redirects to the user" do
+        patch "/users/#{user.id}", params: {user: valid_attributes}
+        user.reload
+        expect(response).to redirect_to(user_url(user))
+      end
     end
   end
 end
