5f85dfa6dfee99c2ce0a37580740d84ea75da673
itsAndL <ayoub003lkebch@gmail.com>
2023-04-09 16:27:38 +0000

    Add Pundit matchers to config file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -103,4 +103,7 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+
+  # Pundit matchers
+  config.include Pundit::Matchers
 end
