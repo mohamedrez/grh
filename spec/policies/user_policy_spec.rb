27f095bb9c25738a3e7b770af22b17344366bf51
itsAndL <ayoub003lkebch@gmail.com>
2023-04-10 12:54:46 +0000

    Testing the user policy

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,27 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe UserPolicy, type: :policy do
-  let(:user) { User.new }
-
   subject { described_class }
+  let(:admin_user) { create(:user, admin: true) }
+  let(:user) { create(:user) }
+  
+  permissions :index? do
+    it "grants access if user is an admin" do
+      expect(subject).to permit(admin_user)
+    end
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "denies access if user is not an admin" do
+      expect(subject).not_to permit(user)
+    end
   end
 
   permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    it "grants access if user is an admin" do
+      expect(subject).to permit(admin_user)
+    end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "grants access if user is the same as the record" do
+      record = user
+      expect(subject).to permit(user, record)
+    end
+
+    it "denies access if user is not an admin or the same as the record" do
+      record = admin_user
+      expect(subject).not_to permit(user, record)
+    end
   end
 
   permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "grants access if user is an admin" do
+      expect(subject).to permit(admin_user)
+    end
+
+    it "denies access if user is not an admin" do
+      expect(subject).not_to permit(user)
+    end
   end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :edit? do
+    it "grants access if user is an admin" do
+      expect(subject).to permit(admin_user)
+    end
+
+    it "denies access if user is not an admin" do
+      expect(subject).not_to permit(user)
+    end
   end
 end
