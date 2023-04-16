5e5e4c6a87e9fc420793e79083df2caffde59cf7
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-04-16 17:14:35 +0000

    test color

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'color' do
+    let(:event) { create :event, eventable: eventable }
+    let(:eventable) { create :time_off_request }
+
+    it 'returns color for event' do
+      expect(event.color).to eq('#853131')
+    end
+  end
+
 end
