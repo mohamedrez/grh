19d890602000e4d9f146fb7122da520511c8e49b
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-04-16 17:00:27 +0000

    fixed specs

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,9 +6,7 @@ class EventsController < ApplicationController
       events_response << {
         id: event.id, title: event.eventable_type, start: event.start_at,
         end: event.end_at,
-        avatar: Rails.application.routes.url_helpers.rails_blob_url(
-          event.user.avatar_url_or_default, only_path: true
-        ),
+        avatar: event.user.avatar_url_or_default,
         user: event.user.full_name, color: event.color
       }
     end
