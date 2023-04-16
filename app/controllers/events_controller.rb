2e881e82c214179402b9f4223ab28629e2686eb1
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-04-16 15:59:15 +0000

    fixed events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   def index
     events_response = []
+    @events = Event.all
     @events.each do |event|
       events_response << {
         id: event.id, title: event.eventable_type, start: event.start_at,
