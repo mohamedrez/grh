73d8e824036fdcb62a8c9f05a5b270f39b71e870
itsAndL <ayoub003lkebch@gmail.com>
2023-04-17 15:53:50 +0000

    Testing the user_requests_controller

--- spec/requests/user_requests_spec.rb
+++ spec/requests/user_requests_spec.rb
@@ -1,17 +1,52 @@
 require "rails_helper"
 
 RSpec.describe "UserRequests", type: :request do
-  let(:user) { create(:user, admin: true) }
-  let(:time_off_request) { create(:time_off_request, user_id: user.id, start_date: Date.today, end_date: (Date.today + 7)) }
+  let(:admin_user) { create(:user, admin: true) }
+  let(:user) { create(:user) }
+  let(:time_off_request) { create(:time_off_request, user_id: admin_user.id, start_date: Date.today, end_date: (Date.today + 7)) }
 
   before do
-    sign_in user
+    sign_in admin_user
   end
 
   describe "GET /index" do
     it "renders a successful response" do
-      get user_user_requests_url(user_id: user.id)
+      get user_user_requests_url(user_id: admin_user.id)
       expect(response).to be_successful
+    end
+  end
+
+  describe "PATCH /user_requests/:id" do
+    context "when the request is approved" do
+      before do
+        patch user_user_request_update_path(user_id: user.id, id: time_off_request.user_request.id, managed_by_id: admin_user.id, state: "approved")
+      end
+
+      it "updates the request with the correct attributes" do
+        time_off_request.user_request.reload
+        expect(time_off_request.user_request.state).to eq("approved")
+        expect(time_off_request.user_request.managed_by_id).to eq(admin_user.id)
+      end
+
+      it "redirects to the user time off request page" do
+        expect(response).to redirect_to(user_time_off_request_path(user_id: user.id, id: time_off_request.id))
+      end
+    end
+
+    context "when the request is rejected" do
+      before do
+        patch user_user_request_update_path(user_id: user.id, id: time_off_request.user_request.id, managed_by_id: user.id, state: "rejected")
+      end
+
+      it "updates the request with the correct attributes" do
+        time_off_request.user_request.reload
+        expect(time_off_request.user_request.state).to eq("rejected")
+        expect(time_off_request.user_request.managed_by_id).to eq(user.id)
+      end
+
+      it "redirects to the user time off request page" do
+        expect(response).to redirect_to(user_time_off_request_path(user_id: user.id, id: time_off_request.id))
+      end
     end
   end
 end
