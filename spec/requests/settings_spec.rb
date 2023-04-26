1d33557bdb9964523d8d8518f8840acce87974cd
itsAndL <ayoub003lkebch@gmail.com>
2023-04-26 13:23:26 +0000

    Removing unnecessary fields from settings page

--- spec/requests/settings_spec.rb
+++ spec/requests/settings_spec.rb
@@ -2,22 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Settings", type: :request do
   let(:user) { create(:user, admin: true) }
-  let(:valid_attributes) do
-    {
-      first_name: "James",
-      last_name: "Clear",
-      email: "me@test.com",
-      job_title: "information_technology"
-    }
-  end
-  let(:invalid_attributes) do
-    {
-      first_name: "",
-      last_name: "",
-      email: "me@test.com",
-      job_title: "information_technology"
-    }
-  end
+  let(:avatar) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'test-image.png'), 'image/png') }
+
+  let(:valid_attributes) { { avatar: avatar } }
 
   before do
     sign_in user
@@ -31,27 +18,16 @@ RSpec.describe "Settings", type: :request do
   end
 
   describe "PATCH #update" do
-    context "with valid parameters" do
-      it "updates the requested settings" do
-        patch "/users/#{user.id}/settings", params: {user: valid_attributes}
-        user.reload
-        expect(user.first_name).to eq("James")
-        expect(user.last_name).to eq("Clear")
-        expect(user.job_title).to eq("information_technology")
-      end
-
-      it "redirects to the user" do
-        patch "/users/#{user.id}/settings", params: {user: valid_attributes}
-        user.reload
-        expect(response).to redirect_to(user_settings_url(user_id: user.id))
-      end
+    it "updates the requested settings" do
+      patch "/users/#{user.id}/settings", params: {user: valid_attributes}
+      user.reload
+      expect(user.avatar).to be_attached
     end
 
-    context "with invalid parameters" do
-      it "renders a response with 422 status (i.e. to display the 'edit' template)" do
-        patch "/users/#{user.id}/settings", params: {user: invalid_attributes}
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
+    it "redirects to the user" do
+      patch "/users/#{user.id}/settings", params: {user: valid_attributes}
+      user.reload
+      expect(response).to redirect_to(user_settings_url(user_id: user.id))
     end
   end
 end
