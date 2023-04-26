d11831df97faf583890bada750f7a964d3f39ebf
itsAndL <ayoub003lkebch@gmail.com>
2023-04-26 17:41:29 +0000

    Small change on the request spec for the experiences controler

--- spec/requests/experiences_spec.rb
+++ spec/requests/experiences_spec.rb
@@ -44,14 +44,14 @@ RSpec.describe "/experiences", type: :request do
         }.to change(Experience, :count).by(1)
       end
 
-      it "redirects to the created experience" do
+      it "redirects to the user profile" do
         post user_experiences_url(user_id: admin_user.id), params: {experience: valid_attributes}
         expect(response).to redirect_to(user_url(admin_user))
       end
     end
 
     context "with invalid parameters" do
-      xit "does not create a new Experience" do
+      it "does not create a new Experience" do
         expect {
           post user_experiences_url(user_id: admin_user.id), params: {experience: invalid_attributes}
         }.to change(Experience, :count).by(0)
