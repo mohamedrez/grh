b26c646e3cb4341ba9a61a29145154596256d64d
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-04-27 14:19:52 +0100

    emergency contact submenu

--- app/controllers/emergency_contacts_controller.rb
+++ app/controllers/emergency_contacts_controller.rb
@@ -1,6 +1,6 @@
 class EmergencyContactsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_locals, only: %i[edit update]
+  before_action :set_locals, only: %i[edit update destroy]
 
   def index
     @user = User.find(params[:user_id])
@@ -21,21 +21,37 @@ class EmergencyContactsController < ApplicationController
     @emergency_contact.user_id = @user.id
 
     if @emergency_contact.save
-      redirect_to user_emergency_contacts_url(@user.id), notice: t("flash.successfully_created")
+      flash.now[:notice] = "Emergency contact successfully created."
+      render turbo_stream: [
+        turbo_stream.prepend("emergency_contacts", @emergency_contact),
+        turbo_stream.replace("notification_alert", partial: "layouts/alert"),
+        turbo_stream.remove("new_emergency_contact")
+      ]
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @emergency_contact.destroy
+    flash.now[:notice] = "emergency contact was successfully destroyed."
+    render turbo_stream: [
+      turbo_stream.remove(@emergency_contact),
+      turbo_stream.replace("notification_alert", partial: "layouts/alert")
+    ]
+  end
   def update
     emergency_contact_params[:user_id] = params[:user_id]
     if @emergency_contact.update(emergency_contact_params)
-      redirect_to user_emergency_contacts_url(params[:user_id]), notice: t("flash.successfully_created")
+      flash.now[:notice] = "User was successfully updated."
+      render turbo_stream: [
+        turbo_stream.replace(@emergency_contact, @emergency_contact),
+        turbo_stream.replace("notification_alert", partial: "layouts/alert")
+      ]
     else
       render :edit, status: :unprocessable_entity
     end
   end
-
   private
 
   def set_locals
