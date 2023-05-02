ff5da39ceb8b39343696f485cd614db93ec9095e
itsAndL <ayoub003lkebch@gmail.com>
2023-05-02 17:49:13 +0100

    Removing Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,7 @@ class User < ApplicationRecord
     :recoverable,
     :rememberable,
     :validatable,
-    :confirmable,
-    :omniauthable, omniauth_providers: [:google_oauth2, :twitter]
+    :confirmable
 
   has_one_attached :avatar, dependent: :destroy
   has_rich_text :about
@@ -56,16 +55,6 @@ class User < ApplicationRecord
   enum :contract, %i[CDD CDI Intern], prefix: :user_contract
   enum :category, %i[cadre non_cadre], prefix: :user_category
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.avatar_url = auth.info.image
-      user.username = auth.info.name
-      user.skip_confirmation!
-    end
-  end
-
   def avatar_url_or_default
     if avatar.attached?
       Rails.application.routes.url_helpers.rails_blob_url(
@@ -81,7 +70,7 @@ class User < ApplicationRecord
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["email", "first_name", "last_name", "employee_number", "first_name_cont_or_last_name_or_email_or_employee_number_cont"]
+    ["email", "first_name", "last_name", "employee_number", "first_name_or_last_name_or_email_or_employee_number_cont"]
   end
 
   def self.ransackable_associations(auth_object = nil)
