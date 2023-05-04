29e90327d7774fbb7dd5abf4551182c05ca881bb
itsAndL <ayoub003lkebch@gmail.com>
2023-05-04 18:27:28 +0100

    Sending an email to the requester after been reviewed

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -1,5 +1,6 @@
 class UserRequest < ApplicationRecord
-  after_create :notify_manager
+  after_create :mailing_manager
+  after_update :mailing_employee, unless: :is_pending?
 
   belongs_to :user
   belongs_to :managed_by, class_name: "User", optional: true
@@ -13,7 +14,18 @@ class UserRequest < ApplicationRecord
 
   private
 
-  def notify_manager
-    RequestMailer.with(request: self).emailing_the_request.deliver_now
+  def is_pending?
+    pending?
+  end
+
+  def mailing_manager
+    employee = User.find(user_id)
+    RequestMailer.with(request: self, request_message: "We wanted to let you know that there is a new request by #{employee.full_name} waiting for you.").mailing.deliver_now
+  end
+
+  def mailing_employee
+    employee = User.find(user_id)
+    manager = User.find(employee.manager_id)
+    RequestMailer.with(request: self, request_message: "We wanted to let you know that your request was #{state} by #{manager.full_name}.").mailing.deliver_now
   end
 end
