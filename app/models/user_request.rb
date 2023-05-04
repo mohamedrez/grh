a32fa6f6e6778db45adcf443b236861fa1520a19
itsAndL <ayoub003lkebch@gmail.com>
2023-05-04 12:37:39 +0100

    Notify the manager

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -1,4 +1,6 @@
 class UserRequest < ApplicationRecord
+  after_create :notify_manager
+
   belongs_to :user
   belongs_to :managed_by, class_name: "User", optional: true
   belongs_to :requestable, polymorphic: true
@@ -6,5 +8,12 @@ class UserRequest < ApplicationRecord
   scope :pending, -> { where(state: "pending") }
   scope :approved, -> { where(state: "approved") }
   scope :rejected, -> { where(state: "rejected") }
+
   enum state: {pending: 0, approved: 1, rejected: 2}
+
+  private
+
+  def notify_manager
+    RequestMailer.with(request: self).emailing_the_request.deliver_now
+  end
 end
