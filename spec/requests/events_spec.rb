90e47d7ade8aff9fba7b07f62d8010beff3f8744
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-05-09 19:55:24 +0100

    increase coverage

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -3,7 +3,12 @@ require "rails_helper"
 RSpec.describe "Events", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/events"
+      create :holiday
+      site = create :site, name: "Site 1"
+      user = create :user, service: :financial, site_id: site.id
+      time_request = create :time_request
+      user_request = create :user_request, user_id: user.id, requestable: time_request
+      get "/events?service=financial&site=1"
       expect(response).to have_http_status(:success)
     end
   end
