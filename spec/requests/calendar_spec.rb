cb5ce300fc9801cd7f70ed0801b422c5e79d429c
itsAndL <ayoub003lkebch@gmail.com>
2023-05-10 16:12:16 +0100

    Modifiy some request specs

--- spec/requests/calendar_spec.rb
+++ spec/requests/calendar_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Calendars", type: :request do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
   describe "GET /index" do
     it "returns http success" do
       get "/calendar"
