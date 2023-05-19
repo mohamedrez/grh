151d171b3bece9ce8d956117408d6fa949a7819c
itsAndL <ayoub003lkebch@gmail.com>
2023-05-19 13:06:34 +0100

    Work on the new and edit for Expense resource

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -21,7 +21,12 @@ class ExpensesController < ApplicationController
     @expense.user_id = @user.id
 
     if @expense.save
-      redirect_to expense_url(@expense), notice: t("flash.successfully_created")
+      flash.now[:notice] = t("flash.successfully_created")
+      render turbo_stream: [
+        turbo_stream.append("expense-list", @expense),
+        turbo_stream.replace("right", partial: "shared/right"),
+        turbo_stream.replace("notification_alert", partial: "layouts/alert")
+      ]
     else
       render :new, status: :unprocessable_entity
     end
@@ -30,7 +35,12 @@ class ExpensesController < ApplicationController
   def update
     expense_params[:user_id] = params[:user_id]
     if @expense.update(expense_params)
-      redirect_to expense_url(@expense), notice: t("flash.successfully_updated")
+      flash.now[:notice] = t("flash.successfully_updated")
+      render turbo_stream: [
+        turbo_stream.replace(@expense, @expense),
+        turbo_stream.replace("right", partial: "shared/right"),
+        turbo_stream.replace("notification_alert", partial: "layouts/alert")
+      ]
     else
       render :edit, status: :unprocessable_entity
     end
@@ -47,6 +57,6 @@ class ExpensesController < ApplicationController
   end
 
   def expense_params
-    params.require(:expense).permit(:date, :category, :description, :amount, :status)
+    params.require(:expense).permit(:date, :category, :description, :amount, :status, receipts: [])
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,10 @@
 class Expense < ApplicationRecord
   belongs_to :user
-  enum :category, ["None", "Travel Expenses", "Meal and Entertainment Expenses", "Training and Development Expenses", "Office Supplies and Equipment Expenses", "Recruitment Expenses", "Employee Benefits Expenses", "Miscellaneous Expenses"], prefix: :category
+
+  has_many_attached :receipts
+
+  enum :category, ["--None--", "Travel Expenses", "Meal and Entertainment Expenses", "Training and Development Expenses", "Office Supplies and Equipment Expenses", "Recruitment Expenses", "Employee Benefits Expenses", "Miscellaneous Expenses"], prefix: :category
   enum status: {pending: 0, approved: 1, rejected: 2, paid: 3}
+
   validates :date, :category, :amount, presence: true
 end
