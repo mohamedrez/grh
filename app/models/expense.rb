1fd73180a72e993cc66d7bf4b3f3822418131de2
itsAndL <ayoub003lkebch@gmail.com>
2023-05-21 12:45:06 +0100

    Fixing an issue and working on gui

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -7,4 +7,20 @@ class Expense < ApplicationRecord
   enum status: {pending: 0, approved: 1, rejected: 2, paid: 3}
 
   validates :date, :category, :amount, presence: true
+
+  def receipts=(attachables)
+    attachables = Array(attachables).compact_blank
+
+    return unless attachables.any?
+    new_attachments = attachables.reject { |attachment| attachment_exists?(attachment) }
+
+    return unless new_attachments.any?
+    attachment_changes["receipts"] = ActiveStorage::Attached::Changes::CreateMany.new("receipts", self, receipts.blobs + new_attachments)
+  end
+
+  private
+
+  def attachment_exists?(attachment)
+    receipts.blobs.any? { |blob| blob.filename.to_s == attachment.original_filename }
+  end
 end
