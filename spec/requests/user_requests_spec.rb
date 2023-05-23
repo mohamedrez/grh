d5ff6a8778ffe8793ae3c4db4c4207a0f65b8c78
itsAndL <ayoub003lkebch@gmail.com>
2023-05-23 14:46:51 +0100

    Working on I18n, rspec, and mobile sidebar

--- spec/requests/user_requests_spec.rb
+++ spec/requests/user_requests_spec.rb
@@ -17,6 +17,13 @@ RSpec.describe "UserRequests", type: :request do
     end
   end
 
+  describe "GET /all_requests" do
+    it "renders a successful response" do
+      get requests_url
+      expect(response).to be_successful
+    end
+  end
+
   describe "PATCH /user_requests/:id" do
     context "when the request is approved" do
       before do
