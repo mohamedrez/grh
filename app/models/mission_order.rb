1d48d5079b22f9157fdca5729d21b81c51ea4e6e
itsAndL <ayoub003lkebch@gmail.com>
2023-06-09 10:43:06 +0100

    Add validation for indemnity_type

--- app/models/mission_order.rb
+++ app/models/mission_order.rb
@@ -8,10 +8,11 @@ class MissionOrder < ApplicationRecord
   enum indemnity_type: {expense_report: 0, flat_rate: 1}
   enum accommodation: {apartment: 0, hotel: 1, other_accommodations: 2}
   enum mission_type: {project: 0, site: 1}
-  enum transport_type: {plane: 0, train: 1, bus: 2}
+  enum transport_type: {plane: 0, train: 1, bus: 2, other_transport_types: 3}
 
   validates :title, :start_date, :end_date, :indemnity_type, :mission_type, :location, :transport_type, presence: true
   validates_comparison_of :end_date, greater_than_or_equal_to: :start_date
+  validate :validate_indemnity_type
 
   delegate :user, to: :user_request
 
@@ -19,5 +20,19 @@ class MissionOrder < ApplicationRecord
 
   def create_user_request
     UserRequest.create(user_id: user_id, state: :pending, requestable: self)
+  end
+
+  private
+
+  def validate_indemnity_type
+    return unless start_date.present? && end_date.present?
+
+    duration = (end_date - start_date).to_i + 1
+
+    if duration == 1 && indemnity_type == "expense_report"
+      errors.add(:indemnity_type, I18n.t("attributes.mission_order.errors.cannot_be_expense_report"))
+    elsif duration > 15 && indemnity_type == "expense_report"
+      errors.add(:indemnity_type, I18n.t("attributes.mission_order.errors.must_be_flat_rate"))
+    end
   end
 end
