7d909a0b7a48b8281265e84930d44bab8be93445
Mehdi <mehdi.elrafi@gmail.com>
2023-06-15 10:54:03 +0100

    Delete migrations

--- db/migrate/20230613095647_add_payment_type_to_mission_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddPaymentTypeToMissionOrders < ActiveRecord::Migration[7.0]
-  def change
-    add_column :mission_orders, :payment_type, :string
-  end
-end
--- db/migrate/20230614162229_remove_attachment_from_job_application.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAttachmentFromJobApplication < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :job_applications, :attachment
-  end
-end
