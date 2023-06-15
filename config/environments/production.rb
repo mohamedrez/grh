87b1bf66697a03f78f51ae79b4c443aa5fb3b3ac
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-06-15 15:29:06 +0200

    fix host set port 8800 for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = {host: "uat.humaneo.ma"}
+  config.action_mailer.default_url_options = {host: "uat.humaneo.ma", port: 8800}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
