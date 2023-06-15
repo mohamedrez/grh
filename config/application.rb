0022b1ef7d9c5ba466231062b504a2bb84dee761
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-06-15 14:56:32 +0200

    comment errors for now

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,9 @@ module Maktabah
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.exceptions_app = ->(env) {
-      ErrorsController.action(:show).call(env)
-    }
+    # config.exceptions_app = ->(env) {
+    #   ErrorsController.action(:show).call(env)
+    # }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
