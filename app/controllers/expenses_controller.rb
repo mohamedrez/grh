8e33ca3be8bb0e88c567ce7346103115dbb0d196
itsAndL <ayoub003lkebch@gmail.com>
2023-06-16 11:56:04 +0100

    Editing the view of TimeRequest && MissionOrder

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,6 @@
 class ExpensesController < ApplicationController
-  before_action :set_expense, only: %i[show edit update destroy update_status delete_receipt]
   before_action :set_user, except: :index
+  before_action :set_expense, only: %i[show edit update destroy update_status delete_receipt]
   before_action :set_breadcrumbs, only: :index
 
   def index
