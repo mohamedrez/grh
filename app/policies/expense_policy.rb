f1889b1f5c703d91e21a8e40f6785ab415a1f3b7
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-06-19 18:27:21 +0200

    fixed spec

--- app/policies/expense_policy.rb
+++ app/policies/expense_policy.rb
@@ -3,7 +3,7 @@ class ExpensePolicy < ApplicationPolicy
 
   def show?
     user.has_any_role?([:hr, :admin]) ||
-      (user.id == record.user_id) ||
+      (user.id == record.user.id) ||
       (user.id == record.user.manager_id)
   end
 
