c87f6e5de9a8642d905285ebd6b2f16979d5a60e
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-06-19 18:46:26 +0200

    sentry only in production

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,5 +3,5 @@ class Role < ApplicationRecord
 
   validates :name, presence: true
 
-  enum name: {hr: 1, admin: 2, accountant: 3, manager: 4}
+  enum name: {hr: 1, admin: 2, accountant: 3, manager: 4, hr_site_manager: 5, accountant_site_manager: 6}
 end
