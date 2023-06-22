cb11678dd9fe81914c9aecf9c433801e8ca33976
itsAndL <ayoub003lkebch@gmail.com>
2023-06-22 10:16:21 +0100

    Fixing the percentages

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -87,7 +87,7 @@ class Goal < ApplicationRecord
   end
 
   def self.summary_stats(manager)
-    goals = Goal.joins(:owner).where(users: {manager_id: manager.id})
+    goals = Goal.joins(:owner).where(users: {manager_id: manager.id}).where(archived: false)
     total_goals = goals.count
     stats = goals.group(:status).count
 
@@ -97,6 +97,16 @@ class Goal < ApplicationRecord
     overpassed_percentage = (stats["overpassed"].to_f / total_goals) * 100
     no_feedback_percentage = (stats[nil].to_f / total_goals) * 100
 
-    {"no_feedback" => no_feedback_percentage.round(2), "not_achieved" => not_achieved_percentage.round(2), "partially_achieved" => partially_achieved_percentage.round(2), "completed" => completed_percentage.round(2), "overpassed" => overpassed_percentage.round(2)}
+    percentages = {
+      "no_feedback" => no_feedback_percentage.round(2),
+      "not_achieved" => not_achieved_percentage.round(2),
+      "partially_achieved" => partially_achieved_percentage.round(2),
+      "completed" => completed_percentage.round(2),
+      "overpassed" => overpassed_percentage.round(2)
+    }
+
+    percentages.each do |key, value|
+      percentages[key] = 0.0 if value.nan?
+    end
   end
 end
