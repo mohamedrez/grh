df5bde7746fad370a6bd6f83e1841d6957834150
MehdiElRafi <mehdi.elrafi@gmail.com>
2023-06-26 10:55:05 +0100

    add test file to model spec folder

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe JobApplication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:job) { create(:job, id: 1) }
+  let(:job_application) { create(:job_application, job_id: job.id) }
+  
+  describe '#applicant_state' do
+    it 'returns the correct hash for different states' do
+      applied_state = job_application.applicant_state("applied")
+      expect(applied_state).to eq({ second_state: "advanced_to_phone", title: I18n.t("attributes.job_application.aasm_states.advance_to_phone") })
+
+      advanced_to_phone_state = job_application.applicant_state("advanced_to_phone")
+      expect(advanced_to_phone_state).to eq({ second_state: "completed_phone", title: I18n.t("attributes.job_application.aasm_states.complete_phone") })
+
+      completed_phone_state = job_application.applicant_state("completed_phone")
+      expect(completed_phone_state).to eq({ second_state: "advanced_interview", title: I18n.t("attributes.job_application.aasm_states.advance_to_interview") })
+
+      advanced_interview_state = job_application.applicant_state("advanced_interview")
+      expect(advanced_interview_state).to eq({ second_state: "completed_interview", title: I18n.t("attributes.job_application.aasm_states.complete_interview") })
+
+      completed_interview_state = job_application.applicant_state("completed_interview")
+      expect(completed_interview_state).to eq({ result: I18n.t("attributes.job_application.aasm_states.qualified"), color: "green" })
+
+      disqualified_state = job_application.applicant_state("disqualified")
+      expect(disqualified_state).to eq({ result: I18n.t("attributes.job_application.aasm_states.disqualified"), color: "red" })
+    end
+  end
 end
