f2a74f9511ae855ed5872a23eb6845057d319bab
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-07-04 08:44:55 +0200

    fixed commentable type

--- app/subscribers/comment_subscriber.rb
+++ app/subscribers/comment_subscriber.rb
@@ -13,6 +13,7 @@ class CommentSubscriber
     # Notify the manager if he is not the author of the comment
     if comment.author_id != comment.commentable.user&.manager_id &&
         comment.commentable.user.manager&.email &&
+        comment.commentable_type == "UserRequest" &&
         comment.one_comment?
       notify_comment(comment, comment.commentable.user.manager.email)
     end
@@ -51,43 +52,6 @@ class CommentSubscriber
       title: title,
       description: description,
       link_url: link_url(comment)
-    )
-  end
-
-  def self.mailing_manager(user_request)
-    return unless user_request.user.manager_id
-
-    subject = title = "Vous avez une nouvelle demande"
-    email = user_request.user.manager&.email
-    link_url = URL_HELPERS.user_time_request_url(user_id: user_request.user.id, id: user_request.requestable_id)
-    link_text = "Voir la demande"
-    description = "L'employé #{user_request.user.full_name} vient de soummettre"
-    description += "  une nouvelle demande de type #{user_request.requestable_type}"
-    NotificationMailerJob.perform_later(
-      user_request,
-      subject: subject,
-      email: email,
-      link_text: link_text,
-      title: title,
-      description: description,
-      link_url: link_url
-    )
-  end
-
-  def self.mailing_owner(user_request)
-    subject = title = "Votre demande vient d'être mis à jour"
-    email = user_request.user.email
-    link_url = URL_HELPERS.user_time_request_url(user_id: user_request.user.id, id: user_request.requestable_id)
-    link_text = "Voir la demande"
-    description = "l'état #{user_request.state} par #{user_request.managed_by.full_name} "
-    NotificationMailerJob.perform_later(
-      user_request,
-      subject: subject,
-      email: email,
-      link_text: link_text,
-      title: title,
-      description: description,
-      link_url: link_url
     )
   end
 end
