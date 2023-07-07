9052beeb9ba825db576e781f3eecfcb86b4e47c8
itsAndL <ayoub003lkebch@gmail.com>
2023-07-07 09:51:00 +0100

    Adding some coverage

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe Expense, type: :model do
       expect(next_states).to eq([:validate_by_hr, :back_to_modify, :reject])
     end
 
-    it 'returns the correct next states for aasm_state with value validated_by_manager' do
+    it 'returns the correct next states for aasm_state with value validated_by_hr' do
       accountant_user = create(:user)
       Role.create!(user_id: accountant_user.id, name: :accountant)
       expense.update!(aasm_state: "validated_by_hr")
@@ -69,10 +69,16 @@ RSpec.describe Expense, type: :model do
       Role.create!(user_id: user_multi_roles.id, name: :hr)
       Role.create!(user_id: user_multi_roles.id, name: :accountant)
 
-      expense.update!(aasm_state: "validated_by_hr")
+      expense.update!(aasm_state: "back_to_modified")
 
       next_states = expense.available_next_states(user_multi_roles)
-      expect(next_states).to eq([:pay, :back_to_modify])
+      expect(next_states).to eq([:validate_by_manager, :back_to_modify, :reject])
+    end
+
+    it 'returns an [] for unsupported states' do
+      expense.aasm_state = 'paid'
+      next_states = expense.available_next_states(user)
+      expect(next_states).to be_empty
     end
   end
 
--- spec/policies/expense_policy_spec.rb
+++ spec/policies/expense_policy_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe ExpensePolicy do
   let(:manager_user) { create(:user) }
   let(:manager_user2) { create(:user) }
   let(:user) { create(:user, manager_id: manager_user.id) }
+  let(:accountant_user) { create(:user) }
   let(:other_user) { create(:user) }
 
   let(:record) { create(:expense, id: 1, user_id: user.id) }
@@ -15,6 +16,7 @@ RSpec.describe ExpensePolicy do
     Role.create!(user_id: hr_user.id, name: :hr)
     Role.create!(user_id: manager_user.id, name: :manager)
     Role.create!(user_id: manager_user2.id, name: :manager)
+    Role.create!(user_id: accountant_user.id, name: :accountant)
   end
 
   describe "#show?" do
@@ -63,6 +65,53 @@ RSpec.describe ExpensePolicy do
         Role.where(user_id: manager_user.id, name: :manager).destroy_all
         is_expected.to eq false
       end
+    end
+  end
+
+  describe "#validate_expense_by_hr?" do
+    subject { policy.apply(:validate_expense_by_hr?) }
+
+    context "when user has hr role, can validate the expense" do
+      let(:policy) { described_class.new(record, user: hr_user) }
+      it { is_expected.to eq true }
+    end
+
+    context "when user hasn't hr role, can't validate the expense" do
+      let(:policy) { described_class.new(record, user: user) }
+      it { is_expected.to eq false }
+    end
+  end
+
+  describe "#pay_expense?" do
+    subject { policy.apply(:pay_expense?) }
+
+    context "when user has accountant role, can pay the expense" do
+      let(:policy) { described_class.new(record, user: accountant_user) }
+      it { is_expected.to eq true }
+    end
+
+    context "when user hasn't hr role, can't pay the expense" do
+      let(:policy) { described_class.new(record, user: user) }
+      it { is_expected.to eq false }
+    end
+  end
+
+  describe "#back_expense_to_modify?" do
+    subject { policy.apply(:back_expense_to_modify?) }
+
+    context "when user has manger role and the manager of the record, can back_expense_to_modify" do
+      let(:policy) { described_class.new(record, user: manager_user) }
+      it { is_expected.to eq true }
+    end
+
+    context "when user has hr role, can back_expense_to_modify" do
+      let(:policy) { described_class.new(record, user: hr_user) }
+      it { is_expected.to eq true }
+    end
+
+    context "when user has accountant role, can back_expense_to_modify" do
+      let(:policy) { described_class.new(record, user: accountant_user) }
+      it { is_expected.to eq true }
     end
   end
 end
