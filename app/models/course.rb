5baf4f70734811302779d0c63d299b4f4a83b6d9
Denys <macbook@dlaptop.local>
2023-07-11 13:10:38 +0300

    Add a course Model with CRUD operations

---
+++ app/models/course.rb
@@ -0,0 +1,4 @@
+class Course < ApplicationRecord
+  validates :title, :description, presence: true
+  has_rich_text :description
+end
---
+++ db/migrate/20230705150121_add_courses_table.rb
@@ -0,0 +1,9 @@
+class AddCoursesTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table(:courses) do |t|
+      t.string(:title)
+      t.string(:description)
+      t.timestamps
+    end
+  end
+end
